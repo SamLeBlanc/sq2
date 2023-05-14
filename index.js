1253b14a0298ac7c43ea91f0384170991d02878c
Cameron MacDonald <53975027+cam-working@users.noreply.github.com>
2023-05-13 23:05:53 -0400

    80% chance this is broken :)

--- index.js
+++ index.js
@@ -1,73 +1,74 @@
 import { applyDragandTouchEvents } from './move.js';
 
-  let dictionary;
-  let puzzles;
-  const loadDictionary = () => {
-    return fetch('words.json') // Return the Promise from fetch
+let dictionary;
+let puzzles;
+let puzzleWords; // new set to store 5-letter words from the current puzzle
+const loadDictionary = () => {
+  return fetch('words.json') // Return the Promise from fetch
+    .then(response => response.json())
+    .then(jsonData => {
+      dictionary = new Set(Object.keys(jsonData));
+    })
+    .catch(error => console.error(error));
+}
+const loadPuzzles = () => {
+    return fetch('puzzles.json') // Return the Promise from fetch
       .then(response => response.json())
       .then(jsonData => {
-        dictionary = new Set(Object.keys(jsonData));
+        puzzles = jsonData;
       })
       .catch(error => console.error(error));
+}
+
+
+const generateTiles = () => {
+  const gameBoard = document.getElementById('game-board');
+  for (let i = 0; i < 49; i++) {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    tile.id = 'tile' + i;
+    gameBoard.appendChild(tile);
   }
-  const loadPuzzles = () => {
-      return fetch('puzzles.json') // Return the Promise from fetch
-        .then(response => response.json())
-        .then(jsonData => {
-          puzzles = jsonData;
-        })
-        .catch(error => console.error(error));
-  }
+};
 
+const buildBoard = tiles => {
+  // Get all keys from the puzzles object
+  let keys = Object.keys(puzzles);
 
-  const generateTiles = () => {
-    const gameBoard = document.getElementById('game-board');
-    for (let i = 0; i < 49; i++) {
-      const tile = document.createElement('div');
-      tile.classList.add('tile');
-      tile.id = 'tile' + i;
-      gameBoard.appendChild(tile);
-    }
-  };
+  // Select a random key
+  let randomKey = keys[Math.floor(Math.random() * keys.length)];
 
-  const buildBoard = tiles => {
-    // Get all keys from the puzzles object
-    let keys = Object.keys(puzzles);
+  // Get the puzzle corresponding to the random key
+  let puzzle = puzzles[randomKey];
+  document.getElementById('puzzle-id').textContent = 'Puzzle ID: ' +  randomKey;
+  let letters = [...puzzle].sort(() => Math.random() - 0.5);
 
-    // Select a random key
-    let randomKey = keys[Math.floor(Math.random() * keys.length)];
-
-    // Get the puzzle corresponding to the random key
-    let puzzle = puzzles[randomKey];
-    document.getElementById('puzzle-id').textContent = 'Puzzle ID: ' +  randomKey;
-    let letters = [...puzzle].sort(() => Math.random() - 0.5);
-
-    // Add every 5-letter word in the puzzle ID to the dictionary
-    for (let i = 0; i < puzzle.length; i += 5) {
-        let word = puzzle.substr(i, 5);
-        if (!dictionary.has(word)) {
-            dictionary.add(word);
-        }
-    }
-
-    // For example, let's assume that each puzzle is an object containing an array of letters and filled indices
-     let filledIndices = [8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40];
-
-    // Assign letters to tiles and color them grey
-    filledIndices.forEach((index, i) => {
-      tiles[index].textContent = letters[i];
-      tiles[index].classList.add('filled');
-    });
-
-    // Assign empty strings to the rest of the tiles and color them white
-    tiles.forEach((tile, i) => {
-      if (!filledIndices.includes(i)) {
-        tile.textContent = '';
-        tile.classList.add('empty');
+  // Initialize puzzleWords and add every 5-letter word in the puzzle ID
+  puzzleWords = new Set(); // initialize puzzleWords
+  for (let i = 0; i < puzzle.length; i += 5) {
+      let word = puzzle.substr(i, 5);
+      if (!puzzleWords.has(word)) {
+          puzzleWords.add(word);
       }
-    });
   }
 
+  // For example, let's assume that each puzzle is an object containing an array of letters and filled indices
+   let filledIndices = [8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40];
+
+  // Assign letters to tiles and color them grey
+  filledIndices.forEach((index, i) => {
+    tiles[index].textContent = letters[i];
+    tiles[index].classList.add('filled');
+  });
+
+  // Assign empty strings to the rest of the tiles and color them white
+  tiles.forEach((tile, i) => {
+    if (!filledIndices.includes(i)) {
+      tile.textContent = '';
+      tile.classList.add('empty');
+    }
+  });
+}
 
   const findWords = () => {
     const gridSize = 7;  // Grid is 7x7
@@ -154,28 +155,61 @@ import { applyDragandTouchEvents } from './move.js';
     document.getElementById('total-score').textContent = score[3];
   }
 
-  const updateBoard = () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
-    const gameBoard = document.getElementById('game-board');
+// Update updateBoard function to handle 'correct-valid' class
+const updateBoard = () => {
+  const tiles = Array.from(document.querySelectorAll('.tile'));
+  const gameBoard = document.getElementById('game-board');
 
-    // Remove 'valid' and 'double-valid' class from all tiles and add 'filled' class
-    tiles.forEach(tile => {
-      tile.classList.remove('valid', 'double-valid','triple-valid');
-      if (tile.textContent.trim() !== "") {
-        tile.classList.add('filled');
+  // Remove 'valid' and 'double-valid' class from all tiles and add 'filled' class
+  tiles.forEach(tile => {
+    tile.classList.remove('valid', 'double-valid','triple-valid', 'correct-valid');
+    if (tile.textContent.trim() !== "") {
+      tile.classList.add('filled');
+    }
 
-    words.forEach(({word, ids, valid}) => {
+    const words = findWords();
+    const tileCounts = {};
+
+    words.forEach(({ids, valid, word}) => { // add 'word' to the destructuring
       if (valid) {
         ids.forEach(id => {
           let tile = document.getElementById(id);
           tileCounts[id] = (tileCounts[id] || 0) + 1;
 
           // Check if the word is one of the 5-letter words in the puzzle ID
-          if (dictionary.has(word) && word.length === 5) {
+          if (puzzleWords.has(word) && word.length === 5) {
             tile.classList.add('correct-valid');
-
+          }
+        });
       }
     });
+
+    Object.entries(tileCounts).forEach(([id, count]) => {
+      let tile = document.getElementById(id);
+      if (count === 1) {
+        tile.classList.add('valid');
+        tile.classList.remove('filled');
+      } else if (count >= 2) {
+        tile.classList.add('double-valid');
+        tile.classList.remove('filled');
+
+        let rect = tile.getBoundingClientRect();
+        let boardRect = gameBoard.getBoundingClientRect();
+        let row = Math.floor((rect.top - boardRect.top) / rect.height);
+        let col = Math.floor((rect.left - boardRect.left) / rect.width);
+
+        if (row >= 1 && row <= 5 && col >= 1 && col <= 5) {
+          tile.classList.remove('double-valid');
+          tile.classList.add('triple-valid');
+        }
+      }
+    });
+
+    updateScoreDisplay();  // Update the score display after highlighting valid words
+
+
+
+
 
     const words = findWords();
     const tileCounts = {};
